050eba6efa51d1429bd948fc6169eb0347f3965d
Andy Baizon <dev@baizon.uk>
2025-02-25 18:06:54 +0000

    feat: serve metrics as html

--- metrics.go
+++ metrics.go
@@ -6,9 +6,9 @@ import (
 )
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	r.Header.Set("Content-Type", "text/plain; charset=utf-8")
+	r.Header.Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	bodyContent := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	bodyContent := fmt.Sprintf("<html>\n    <body>\n        <h1>Welcome, Chirpy Admin</h1>\n        <p>Chirpy has been visited %d times!</p>\n    </body>\n</html>", cfg.fileserverHits.Load())
 	w.Write([]byte(bodyContent))
 }
 
