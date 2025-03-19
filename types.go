f513a4a3f2b44d491f37d0793bd83cc23c25d13f
Andy Baizon <dev@baizon.uk>
2025-03-19 07:59:57 +0000

    feat: add type for Chirp

--- types.go
+++ types.go
@@ -20,3 +20,11 @@ type User struct {
 	UpdatedAt time.Time `json:"updated_at"`
 	Email     string    `json:"email"`
 }
+
+type Chirp struct {
+	Id        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body      string    `json:"body"`
+	UserId    uuid.UUID `json:"user_id"`
+}
